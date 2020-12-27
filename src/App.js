10fa93c3054414eadb4e3c4036e948a4d9806a71
adrianoduque97 <adrianoduque97@gmail.com>
2020-12-27 17:15:14 -0500

    ADA -> Created app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FeynurLogo  from './Feyneur1.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={FeynurLogo} className="App-logo" alt="logo"/>
+          Loading...
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Fey  from '../LDU.png'
+
+const TestComp = () =>(
+    <div>
+        <img src={Fey} alt=""/>
+    </div>
+)
+
+export default TestComp
