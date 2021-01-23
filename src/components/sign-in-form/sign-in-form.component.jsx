69ddab526bd6199955a5aa9053fa21db32c7fb79
adrianoduque97 <adrianoduque97@gmail.com>
2021-01-23 12:40:46 -0500

    ADA -> Added routes and new PDF

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import {Form, Button, Card} from 'react-bootstrap'
+import { useHistory } from "react-router-dom";
 
 
 const SignInForm = () =>{
+
+    const history = useHistory();
+
+    const routeChange = () =>{ 
+      let path = `visualizer`; 
+      history.push(path);
+    }
 
     return(
 
@@ -23,7 +31,7 @@ const SignInForm = () =>{
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" onClick={routeChange}>
                         SIGN IN
             </Button>
                 </Form>
